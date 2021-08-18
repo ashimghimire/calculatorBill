2aafe74923ccd6b80c5935a6d562b3284a55d68b
Asim Ghimire <asimghimire@Asims-MacBook-Air.local>
2021-08-18 19:16:22 +0545

    Changed file

--- src/App.js
+++ src/App.js
@@ -3,22 +3,74 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="container wrapper">
+    <div className="header">
+      <img src="./images/logo.svg"  alt="logo-image" className="img-respon"/>
     </div>
+    <div className="calculator-body">
+      <div className="row">
+        <div className="col-sm-6 control">
+          Bill
+          <div className="form-group mt-2">
+            <input type="number" className="bill-input form-control" />
+          </div>
+          <div className="form-group mt-4">
+            Select Tip %
+            <div className="grid-container-button mt-3">
+              <div className="grid-item">
+                <button className="btn btn-default">5%</button>
+              </div>
+              <div className="grid-item">
+                <button className="btn btn-default">5%</button>
+              </div>
+              <div className="grid-item">
+                <button className="btn btn-default">5%</button>
+              </div>
+              <div className="grid-item">
+                <button className="btn btn-default">5%</button>
+              </div>
+              <div className="grid-item">
+                <button className="btn btn-default">5%</button>
+              </div>
+              <div className="grid-item">
+                <button className="btn btn-default">5%</button>
+              </div>
+            </div>
+          </div>
+          <div className="form-group mt-4">
+            Number of People
+            <div className="form-group mt-2">
+              <input type="number" className="bill-input user form-control" />
+            </div>
+          </div>
+
+        </div>
+        <div className="col-md-6 display p-5">
+          <div className="container m-0 p-0">
+          <div className="row">
+            <div className="col-6 text-start">
+              <div className="label">Tip amount</div>
+              <div className="sub-label">/person</div>
+            </div>
+            <div className="col-6 text-end">
+              <div className="amount">$0.00</div>
+            </div>
+          </div>
+          <div className="row mt-5">
+            <div className="col-6 text-start">
+              <div className="label">Tip amount</div>
+              <div className="sub-label">/person</div>
+            </div>
+            <div className="col-6 text-end">
+              <div className="amount">$0.00</div>
+            </div>
+          </div>
+        </div>
+        <button className="btn btn-default mt-5">Reset</button>
+        </div>
+        </div>
+        </div>
+  </div>
   );
 }
 
